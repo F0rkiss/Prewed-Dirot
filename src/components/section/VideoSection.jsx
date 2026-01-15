3c9e7adf6c2b0ea2df433e792a1000ce8288b665
Damar Pradnyana <134548390+F0rkiss@users.noreply.github.com>
2026-01-15 14:03:27 +0700

    video change path

--- src/components/section/VideoSection.jsx
+++ src/components/section/VideoSection.jsx
@@ -1,4 +1,4 @@
-import Video from '../../assets/video/sample.mp4';
+// import Video from '../../assets/video/sample.mp4';
 
 const VideoSection = () => {
   return (
@@ -9,9 +9,9 @@ const VideoSection = () => {
         </div>
         <div className="mb-2 text-sm text-gray-300 tracking-widest">
         <video width="640" height="360" controls>
-            <source src={Video} type="video/mp4" />
+            {/* <source src={Video} type="video/mp4" />
                 Your browser does not support the video tag.
-            </video>
+            </video> */}
         </div>
       </div>
     </section>
